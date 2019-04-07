a1d7489a4b482bcaa68713d64722b0cc8861beed
John Barrett <44798179+JohnJBarrett22@users.noreply.github.com>
2019-04-06 21:30:19 -0700

    Added all.html, additional ajax functionality and finished out the 1st demo section

--- Django/ajax_demo/apps/demo_app/urls.py
+++ Django/ajax_demo/apps/demo_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('all.json', views.all_json),
+    path('all.html', views.all_html)
+
 ]
--- Django/ajax_demo/apps/demo_app/views.py
+++ Django/ajax_demo/apps/demo_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
-# from models import *
+from .models import *
+from django.core import serializers
 
 def index(request):
     return render(request, "demo_app/index.html")
+
+def all_json(request):
+    users = User.objects.all()
+    users_json = serializers.serialize("json", users)
+    print (users_json)
+    return HttpResponse(users_json, content_type='application/json')
+
+def all_html(request):
+    users = User.objects.all()
+    return render(request, "demo_app/all.html", {"users": users})
