b0ca5d9cc7c992911bbf2beea743c3a91f8d9a76
John Barrett <44798179+JohnJBarrett22@users.noreply.github.com>
2019-04-06 23:26:03 -0700

    Cleaned up odd spacing issues, still pending refactoring of code

--- Django/ajax_demo/apps/demo_app/views.py
+++ Django/ajax_demo/apps/demo_app/views.py
@@ -25,5 +25,3 @@ def create(request):
     User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email_address=request.POST['email_address'])
     users = User.objects.all()
     return render(request, "demo_app/all.html", {"users": users})
-
-
